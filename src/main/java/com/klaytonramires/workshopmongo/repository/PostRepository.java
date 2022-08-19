adadb16e0392090604fa0540ad57ffb8c50a86aa
kramires <ramires03@hotmail.com>
2022-08-19 17:39:15 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/klaytonramires/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.klaytonramires.workshopmongo.repository;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.klaytonramires.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository< Post, String> {
+	
+}
