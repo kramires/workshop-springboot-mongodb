6ca8752fce453daf51d5cff8220e5a3ffb726ea6
kramires <ramires03@hotmail.com>
2022-08-25 16:56:06 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/klaytonramires/workshopmongo/domain/Post.java
+++ src/main/java/com/klaytonramires/workshopmongo/domain/Post.java
@@ -2,12 +2,15 @@ package com.klaytonramires.workshopmongo.domain;
 
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.klaytonramires.workshopmongo.dto.AuthorDTO;
+import com.klaytonramires.workshopmongo.dto.CommentDTO;
 
 
 @Document
@@ -21,7 +24,7 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 	
-	//private List<CommentDTO> comments = new ArrayList<>();
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -75,13 +78,13 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
-//	public List<CommentDTO> getComments() {
-//		return comments;
-//	}
-//
-//	public void setComments(List<CommentDTO> comments) {
-//		this.comments = comments;
-//	}
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
